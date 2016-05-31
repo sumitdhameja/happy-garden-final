f912a44163b0c2f41692d7b771f5d70cbe0c589c
Sumeet Dhameja <sumitdhameja@gmail.com>
2016-05-31 01:39:38 -0700

    Added Banner and fixed Modal

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-   
+   $('.subscribeClass').click(function(){
+   $('.modal-body').empty();
+  	var title = "Happy Garden Subscription";
+  	$('.modal-title').html(title);
+
+  	$($('#subscribeForm').html()).appendTo('.modal-body');
+  	$('#myModal').modal({show:true});
+});
   
   
 $('.thumbnail').click(function(){
